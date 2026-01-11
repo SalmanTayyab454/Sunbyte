de53fdce19c7cb30db70cb633e70a9080562bf30
SalmanTayyab <salmantayyab454@gmail.com>
2026-01-11 04:00:12 +0100

    Final changes
    Deploy the complete site

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// Configuration file - DO NOT COMMIT TO PUBLIC REPOSITORY
+// Add this file to .gitignore
+
+const CONFIG = {
+    // Web3Forms Access Key
+    web3formsAccessKey: '2208e33f-9816-47d9-a163-67bfd977f810'
+};
